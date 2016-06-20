60258677a0b927db897cc72d99e36633b603628a
Yoonsik <yoonsik@Yoonsiks-Mac.local>
2016-06-21 00:52:55 +0900

    Mobibench

---
+++ Mobibench_for_ios/Mobibench_for_iosTests/Mobibench_for_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mobibench_for_iosTests.swift
+//  Mobibench_for_iosTests
+//
+//  Created by Yoonsik on 6/21/16.
+//  Copyright © 2016 yoonsik. All rights reserved.
+//
+
+import XCTest
+@testable import Mobibench_for_ios
+
+class Mobibench_for_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
